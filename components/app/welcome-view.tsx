8b67d4470bfe507964c5f49662a2d80e71ec6b63
Masum Hasan <masumhasan92@gmail.com>
2025-10-28 04:58:54 +0600

    update

--- components/app/welcome-view.tsx
+++ components/app/welcome-view.tsx
@@ -44,14 +44,14 @@ export const WelcomeView = ({
 
       <div className="fixed bottom-5 left-0 flex w-full items-center justify-center">
         <p className="text-muted-foreground max-w-prose pt-1 text-xs leading-5 font-normal text-pretty md:text-sm">
-          Visit my Website for more details{' '}
+         All Right Reserved {' '}
           <a
             target="_blank"
             rel="noopener noreferrer"
             href="https://masumhasan.github.io/"
             className="underline"
           >
-            NUR HASAN MASUM
+            Nur Hasan Masum
           </a>
           .
         </p>
